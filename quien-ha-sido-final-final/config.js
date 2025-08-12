3eed99b78b3b86753cccd7a86c5c82a17819382f
Deiibyy <davilitoo03mm@gmail.com>
2025-08-12 02:21:59 +0200

    Delete quien-ha-sido-final-final directory

--- quien-ha-sido-final-final/config.js
+++
@@ -1,5 +0,0 @@
-// Lista de canciones que puedes reemplazar colocando archivos en public/music/
-const canciones = [
-  { nombre: "aitana_superestrella", archivo: "aitana_superestrella.mp3" },
-  { nombre: "Ejemplo Aitana", archivo: "aitana_ejemplo.mp3" }
-];
